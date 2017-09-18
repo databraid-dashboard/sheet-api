ae13c985cbbfa3c9e45be78261478bf7f47c32ec
Meghan Prestemon <m.m.hares@gmail.com>
2017-09-18 10:24:15 -0700

    Add spreadsheet and sheet resolvers (#2)
    * Create mock data and resolvers
    
    * Update models
    
    * Start creating tests
    
    * Update Spreadsheet test
    
    * Complete Sheet model tests
    
    * Update Sheet tests
    
    * Complete RootResolver tests
    
    * Add comments for future developers

--- src/models/Sheet.js
+++ src/models/Sheet.js
@@ -1,13 +1,8 @@
-const Row = require('./Row');
-
 class Sheet {
   constructor(sheet) {
-    this.sheet = sheet;
-    this.range = this.sheet.range;
-    this.values = this.sheet.values;
-  }
-  get rows() {
-    return this.values.map(row => new Row(row));
+    this.range = sheet.range;
+    this.majorDimension = sheet.majorDimension;
+    this.values = sheet.values;
   }
 }
 
---
+++ test/Spreadsheet.test.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef, no-unused-expressions */
+const { expect } = require('chai');
+const Spreadsheet = require('../src/models/Spreadsheet');
+const Sheet = require('../src/models/Sheet');
+
+//TODO: ALL tests will need to be refactored when the backend is making calls
+      // to the Google API
+
+describe('Spreadsheet model', () => {
+  const spreadsheet = new Spreadsheet('1UcfQsQGTAAtjvyxv948z3hf0qiUnMNZF90-GcD7MF9g');
+
+  it('should work', () => {
+    expect(true).to.be.ok;
+  });
+
+  it('accepts spreadsheetId as a passed in argument', () => {
+    const sheets = spreadsheet.sheets;
+    expect(sheets[0]).to.be.an.instanceof(Sheet);
+  });
+});
