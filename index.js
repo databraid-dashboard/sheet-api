0c5ace7b8c5896f00de24b8a49d2c6d1c7d04b9c
Grant <emailgdw@gmail.com>
2017-09-19 12:33:47 -0700

    OAuth Feature using passportJS (#3)
    * Begin Passport google OAuth strategy
    
    * Complete OAuth strategy
    
    * Update .env with  API_BASE_URL

--- index.js
+++ index.js
@@ -1,43 +1,83 @@
+const passport = require('passport');
+require('dotenv').config();
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const graphqlHTTP = require('express-graphql');
 const root = require('./src/resolvers/RootResolver');
 const schema = require('./src/schema');
-
+const partials = require('express-partials');
+const cors = require('cors');
 const app = express();
+
 const PORT = process.env.PORT || 8000;
-
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_SHEETS_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_SHEETS_CLIENT_SECRET;
+const API_BASE_URL= process.env.API_BASE_URL;
+/* eslint-disable no-unused-vars */
+// NOTE these variables will be used once the OAuth strategy begins, not before
+const CALLBACK_URL = process.env.GOOGLE_SHEETS_URL;
+const PROFILE_ID = process.env.GOOGLE_SHEETS_PROFILE_ID;
+app.use('/', partials());
+app.use(cors());
 app.use(bodyParser.json());
-app.use((req, res, next) => {
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+app.use(cookieParser());
 
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-
-  if (req.method === 'OPTIONS') {
-    return res.status(200).end();
-  }
-
-  return next();
+passport.serializeUser((user, done) => {
+  done(null, user);
 });
-app.use(
-  '/graphql',
-  graphqlHTTP({
-    schema,
-    rootValue: root,
-    graphiql: true,
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
+
+app.get('/', (req, res) => {
+  res.send('aok');
+});
+app.use(session({
+  secret: process.env.SECRET,
+  resave: true,
+  saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new GoogleStrategy({
+  clientID: GOOGLE_CLIENT_ID,
+  clientSecret: GOOGLE_CLIENT_SECRET,
+  callbackURL: `${API_BASE_URL}/auth/google/callback`,
+
+},
+  ((accessToken, refreshToken, profile, done) => {
+    process.env.TKN = accessToken;
+    process.env.PROFILE_ID = profile.id;
+    process.env.USERNAME = profile.displayName;
+
+    done(null, profile);
   }),
-);
+));
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login', 'https://www.googleapis.com/auth/spreadsheets.readonly'] }));
+
+app.get('/auth/google/callback',
+  passport.authenticate('google'),
+  (req, res) => {
+    res.cookie('userName', process.env.USERNAME);
+    res.cookie('profileID', process.env.PROFILE_ID);
+    res.redirect(process.env.FRONT_END_BASE_URL);
+  });
+
+  app.use(
+    '/graphql',
+    graphqlHTTP({
+      schema,
+      rootValue: root,
+      graphiql: true,
+    }),
+  );
 
 app.use('/', (req, res) => {
   res.sendStatus(200);
