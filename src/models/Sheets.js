38df4280f3070da362d7d93c6aea9a224facee0d
Michael Murray <michaelmurray6298@gmail.com>
2017-09-21 16:02:33 -0700

    Add intregration to google API (#6)
    * Add intregration to google API
    
    * Remove .env
    
    * Add .catch statements

--- src/models/Sheets.js
+++ src/models/Sheets.js
@@ -1,17 +1,39 @@
-const data = require('../mockdata');
 const Sheet = require('./Sheet');
+const fetch = require('isomorphic-fetch');
 /* eslint-disable class-methods-use-this */
+function getJson(response) {
+  return response.json();
+}
+function createSheetInstances(sheets) {
+  return sheets.map(sheet => new Sheet(sheet));
+}
 class Sheets {
   constructor({ spreadsheetId, sheetName }) {
     this.spreadsheetId = spreadsheetId;
     this.sheetName = sheetName;
+    this.sheetUrl = `https://sheets.googleapis.com/v4/spreadsheets/${this.spreadsheetId}/values/${sheetName}!A1:ZZ?access_token=${process.env.TKN}`;
+    this.spreadSheetUrl = `https://sheets.googleapis.com/v4/spreadsheets/${this.spreadsheetId}?access_token=${process.env.TKN}`;
   }
   get sheets() {
-    return this.sheetName ? [new Sheet(data.SHEET1_DATA)] : this.getAllUserSheets();
+    return this.sheetName ? this.getSheetByName() : this.getAllUserSheets();
+  }
+  getSheetByName() {
+    return fetch(this.sheetUrl)
+      .then(response => getJson(response))
+      .then(data => [new Sheet(data)])
+      .catch(err => err);
   }
   getAllUserSheets() {
-    const sheetDataArr = [data.SHEET1_DATA, data.SHEET2_DATA];
-    return sheetDataArr.map(sheetData => new Sheet(sheetData));
+    return fetch(this.spreadSheetUrl)
+      .then(getJson)
+      .then(response =>
+        response.sheets.map(sheet => sheet.properties.title))
+      .then(sheetNames => sheetNames.map(sheetName => fetch(`https://sheets.googleapis.com/v4/spreadsheets/${this.spreadsheetId}/values/${sheetName}!A1:ZZ?access_token=${process.env.TKN}`)))
+      .then(promiseArray => Promise.all(promiseArray))
+      .then(responses => responses.map(response => response.json()))
+      .then(responses => Promise.all(responses))
+      .then(createSheetInstances)
+      .catch(err => err);
   }
 }
 
--- test/Spreadsheet.test.js
+++ test/Spreadsheet.test.js
@@ -1,20 +1,16 @@
 /* eslint-disable no-undef, no-unused-expressions */
 const { expect } = require('chai');
 const Spreadsheet = require('../src/models/Spreadsheet');
-const Sheet = require('../src/models/Sheet');
-
 // TODO: ALL tests will need to be refactored when the backend is making calls
 // to the Google API
 
 describe('Spreadsheet model', () => {
-  const spreadsheet = new Spreadsheet('1UcfQsQGTAAtjvyxv948z3hf0qiUnMNZF90-GcD7MF9g');
-
   it('should work', () => {
     expect(true).to.be.ok;
   });
 
-  it('accepts spreadsheetId as a passed in argument', () => {
-    const sheets = spreadsheet.sheets;
-    expect(sheets[0]).to.be.an.instanceof(Sheet);
+  it('should take in a spreadsheetId', () => {
+    const spreadsheet = new Spreadsheet({ spreadsheetId: '1UcfQsQGTAAtjvyxv948z3hf0qiUnMNZF90-GcD7MF9g' });
+    expect(spreadsheet.spreadsheetId).to.equal('1UcfQsQGTAAtjvyxv948z3hf0qiUnMNZF90-GcD7MF9g');
   });
 });
